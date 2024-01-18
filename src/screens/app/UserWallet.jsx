0afb1ab2c923de6ce341dd9abfa95a04c23089a1
Ashish Ranjan <52354895+ashish-monal@users.noreply.github.com>
2024-01-18 22:04:18 +0530

    Wallet Section

--- src/screens/app/UserWallet.jsx
+++ src/screens/app/UserWallet.jsx
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Data from '../../assets/data/Transaction.json';
 
 const UserWallet = () => {
+  const renderTransactions = () => {
+    return Data.map((item) => (
+      <View style={{marginBottom:20}} key={item.id}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>
+          {item.date}
+        </Text>
+        {item.transaction.map((transaction) => (
+          <View key={transaction.id} style={{ flexDirection: 'row', justifyContent: 'space-between',padding:10 ,borderWidth:2,borderRadius:10,marginVertical:5}}>
+            <Text style={styles.text}>{transaction.name}</Text>
+            <Text style={[styles.text, { color: transaction.type === 'credit' ? 'green' : 'red' }]}>
+              {transaction.amount}
+            </Text>
+          </View>
+        ))}
+      </View>
+    ));
+  };
+
   return (
-    <View>
-      <Text>UserWallet</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'dark-content'} backgroundColor="white" />
+      {/* Wallet Section */}
+      <View style={styles.walletView}>
+        <View style={styles.viewHolder}>
+          <Text style={styles.text}>Available Balance</Text>
+          <Text style={[styles.text, { color: 'white' }]}>Rs. 599</Text>
+        </View>
+        <View style={{ marginHorizontal: 25 }}>
+          <Entypo name="squared-plus" size={50} color="white" />
+        </View>
+      </View>
+      {/* Transaction Section */}
+      <ScrollView style={[styles.viewHolder,{marginBottom:10,flex:1}]}>
+        <Text style={styles.text}>Transaction</Text>
+       
+        {renderTransactions()}
+       
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default UserWallet
+export default UserWallet;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  walletView: {
+    borderWidth: 2,
+    backgroundColor: '#add8e6',
+    width: '90%',
+    alignSelf: 'center',
+    marginVertical: 20,
+    borderRadius: 20,
+    borderColor: '#add8e6',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  viewHolder: { padding: 20, gap: 20 },
+  text: { fontSize: 23, fontWeight: '600', color: 'black' },
+});
